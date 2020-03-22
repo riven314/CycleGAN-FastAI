09bd859b8189d1a75edce0c5830ae02ce7d2d341
riven314 <alexlauwh@gmail.com>
2020-03-22 20:25:15 +0800

    learnt how callbacks propagate loss information

--- src/model_utils/callbacks.py
+++ src/model_utils/callbacks.py
@@ -1,5 +1,6 @@
 from pdb import set_trace
 from fastai.vision import *
+from fastai.callbacks.tensorboard import LearnerTensorboardWriter, ImageTBWriter
 
 class CycleGANTrainer(LearnerCallback):
     _order = -20 #Need to run before the Recorder
@@ -72,6 +73,123 @@ class CycleGANTrainer(LearnerCallback):
     def on_epoch_end(self, last_metrics, **kwargs):
         # last_metrics is None
         # add_metrics is for updating last_metrics
+        set_trace()
         return add_metrics(last_metrics, 
-                           [s.smooth for s in [self.id_smter,self.gen_smter,self.cyc_smter, self.da_smter,self.db_smter]]
+                           [s.smooth for s in [
+                               self.id_smter, self.gen_smter, self.cyc_smter, self.da_smter,self.db_smter
+                           ]]
                           )
+    
+
+class GANTensorboardWriter(LearnerTensorboardWriter):
+    """
+    Callback for GANLearners that writes to Tensorboard.
+    Extends LearnerTensorboardWriter and adds output image writes.
+    
+    :param:
+        hist_iters : iteration to update model weights histogram 
+        loss_iters : iteration to update loss value
+        stats_iters : iteration to update gradient statistics
+        visual_iters : iteration to update generator
+    """
+    def __init__(self, learn, base_dir, name, loss_iters, hist_iters, stats_iters, visual_iters):
+        super().__init__(learn = learn, base_dir = base_dir, name = name, 
+                         loss_iters = loss_iters, hist_iters=hist_iters, 
+                         stats_iters = stats_iters)
+        self.visual_iters = visual_iters
+        self.img_gen_vis = ImageTBWriter()
+        self.gen_stats_updated = True
+        self.crit_stats_updated = True
+
+    def _write_weight_histograms(self, iteration):
+        """ 
+        Writes model weight histograms to Tensorboard.
+        update self.hist_writer (HistogramTBWriter, inherented from LearnerTensorboardWriter)
+        """
+        G_A, G_B = self.learn.model.G_A, self.learn.model.G_B
+        D_A, D_B = self.learn.model.D_A, self.learn.model.D_B
+        self.hist_writer.write(model = G_A, iteration = iteration, 
+                               tbwriter = self.tbwriter, name = 'G_A_histogram')
+        self.hist_writer.write(model = G_B, iteration = iteration,
+                               tbwriter = self.tbwriter, name = 'G_B_histogram')
+        self.hist_writer.write(model = D_A, iteration = iteration, 
+                               tbwriter = self.tbwriter, name = 'D_A_histogram')
+        self.hist_writer.write(model = D_B, iteration = iteration,
+                               tbwriter = self.tbwriter, name = 'D_B_histogram')
+
+    def _write_gen_model_stats(self, iteration:int):
+        """ 
+        Writes gradient statistics for generator to Tensorboard.
+        update self.stats_writer (ModelStatsTBWriter)
+        """
+        G_A = self.learn.G_A
+        G_B = self.learn.G_B
+        self.stats_writer.write(model = G_A, iteration = iteration, 
+                                tbwriter = self.tbwriter, name = 'G_A_model_stats')
+        self.stats_writer.write(model = G_B, iteration = iteration,
+                                tbwriter = self.tbwriter, name = 'G_B_model_stats')
+        self.gen_stats_updated = True
+
+    def _write_critic_model_stats(self, iteration):
+        """
+        Writes gradient statistics for critic to Tensorboard.
+        update self.stats_writer (ModelStatsTBWriter)
+        """
+        D_A = self.learn.D_A
+        D_B = self.learn.D_B
+        self.stats_writer.write(model = D_A, iteration = iteration, 
+                                tbwriter = self.tbwriter, name = 'D_A_model_stats')
+        self.stats_writer.write(model = D_B, iteration = iteration,
+                                tbwriter = self.tbwriter, name = 'D_B_model_stats')
+        self.crit_stats_updated = True
+
+    def _write_model_stats(self, iteration):
+        "Writes gradient statistics to Tensorboard."
+        # We don't want to write stats when model is not iterated on and hence has zeroed out gradients
+        gen_mode = self.learn.gan_trainer.gen_mode
+        if gen_mode and not self.gen_stats_updated:
+            self._write_gen_model_stats(iteration = iteration)
+        if not gen_mode and not self.crit_stats_updated: 
+            self._write_critic_model_stats(iteration = iteration)
+
+    def _write_training_loss(self, iteration, last_loss)->None:
+        """
+        Writes training loss to Tensorboard. 
+        note last_loss is a list, NOT scalar
+        self.metrics_root = '/metrics/'
+        """
+        recorder = self.learn.gan_trainer.recorder
+        if len(recorder.losses) == 0: return None
+        scalar_value = to_np((recorder.losses[-1:])[0])
+        
+        self.id_smter, self.gen_smter, self.cyc_smter, self.da_smter,self.db_smter
+        tag = self.metrics_root + 'train_loss'
+        self.tbwriter.add_scalar(
+            tag = tag, scalar_value = scalar_value, global_step = iteration
+            )
+
+    def _write_images(self, iteration:int)->None:
+        "Writes model generated, original and real images to Tensorboard."
+        trainer = self.learn.gan_trainer
+        #TODO:  Switching gen_mode temporarily seems a bit hacky here.  Certainly not a good side-effect.  Is there a better way?
+        gen_mode = trainer.gen_mode
+        try:
+            trainer.switch(gen_mode=True)
+            self.img_gen_vis.write(learn=self.learn, trn_batch=self.trn_batch, val_batch=self.val_batch, 
+                                    iteration=iteration, tbwriter=self.tbwriter)
+        finally: trainer.switch(gen_mode=gen_mode)
+
+    def on_batch_end(self, iteration:int, train:bool, **kwargs)->None:
+        "Callback function that writes batch end appropriate data to Tensorboard."
+        super().on_batch_end(iteration=iteration, train=train, **kwargs)
+        if iteration == 0 and not train: return
+        if iteration % self.visual_iters == 0: self._write_images(iteration=iteration)
+
+    def on_backward_end(self, iteration:int, train:bool, **kwargs)->None:
+        "Callback function that writes backward end appropriate data to Tensorboard."
+        if iteration == 0 and not train: return
+        self._update_batches_if_needed()
+        #TODO:  This could perhaps be implemented as queues of requests instead but that seemed like overkill. 
+        # But I'm not the biggest fan of maintaining these boolean flags either... Review pls.
+        if iteration % self.stats_iters == 0: self.gen_stats_updated, self.crit_stats_updated = False, False
+        if not (self.gen_stats_updated and self.crit_stats_updated): self._write_model_stats(iteration=iteration)
